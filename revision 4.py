1801fdf5d545c4670c138ea5c7d053cc1517e697
Sam Cardin <blucardin@gmail.com>
2020-09-16 18:38:53 -0400

    fixed starting direction and game clock
    fixed the starting direction so that you cant just wiz off screen
    
    also fixed the base clock so that it runs at the same speed everywhere
    
    minor impoovments
    
    the end

--- revision 4.py
+++ revision 4.py
@@ -7,34 +7,54 @@ pygame.init()
 
 pygame.display.set_caption("Snake")
 
-global linewidth
 linewidth = 1
 
-global sizex 
-global sizey 
 sizex = 30
 sizey = 30
 
-global color
 color = (225,225 , 225 )
-
-global squaresize
 
 if sizex > sizey or sizey == sizex:
     squaresize = int( ( 979 - 23) /sizex)
 else:
     squaresize = int(1680 /sizey)
 
-global winxlength
 winxlength = sizex * squaresize
-global winywidth
 winywidth = sizey * squaresize
 
-global win
 win = pygame.display.set_mode((winywidth + linewidth, winxlength + linewidth))
 
-global strawberry
-straberry = pygame.image.load("pixil-frame-0.png")
+straberryraw = pygame.image.load("pixil-frame-0.png")
+straberry = pygame.transform.scale(straberryraw, (squaresize, squaresize))
+
+timekeeper = 0
+
+class snakeblock:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        pygame.draw.rect(win, (0, 0, 225), (self.x * squaresize, self.y * squaresize, squaresize, squaresize))
+
+    def redanimation(self):
+        pygame.draw.rect(win, (225, 0, 0), (self.x * squaresize, self.y * squaresize, squaresize, squaresize))
+
+    def winanumation(self):
+        pygame.draw.rect(win, (234, 104, 116), (self.x * squaresize, self.y * squaresize, squaresize, squaresize))
+
+class fruitblock:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y 
+        self.color = color
+    def draw(self):
+        
+        if self.easteregg == False:
+            pygame.draw.rect(win, self.color, (self.x * squaresize, self.y * squaresize, squaresize, squaresize))
+        else:
+            win.blit(straberry, (self.x * squaresize, self.y * squaresize))
+
 
 def titlescreen():
 
@@ -48,11 +68,10 @@ def titlescreen():
     textRect2 = text2.get_rect()
     textRect2.center = (winxlength / 2, (winywidth / 8) * 6) 
 
-
     run = True
     while run:
         win.fill((0,0,0))
-        
+
         if pygame.mouse.get_pressed()[0] == True:
             run = False
 
@@ -71,7 +90,6 @@ def titlescreen():
         pygame.draw.rect(win, (0,0,0), textRect2)
         
         win.blit(text2, textRect2)
-
         win.blit(text, textRect)
 
         pygame.display.update()  
@@ -79,36 +97,7 @@ def titlescreen():
 def snake():
 
     pygame.mouse.set_visible(False)
-
-    class snakeblock:
-        def __init__(self, x, y):
-            self.x = x
-            self.y = y
-
-        def draw(self):
-            pygame.draw.rect(win, (0, 0, 225), (self.x * squaresize, self.y * squaresize, squaresize, squaresize))
-
-        def redanimation(self):
-            pygame.draw.rect(win, (225, 0, 0), (self.x * squaresize, self.y * squaresize, squaresize, squaresize))
-
-        def winanumation(self):
-            pygame.draw.rect(win, (234, 104, 116), (self.x * squaresize, self.y * squaresize, squaresize, squaresize))
-
-
-    class fruitblock:
-        def __init__(self, x, y, color):
-            self.x = x
-            self.y = y 
-            self.color = color
-        def draw(self):
-            
-            if self.easteregg == False:
-                pygame.draw.rect(win, self.color, (self.x * squaresize, self.y * squaresize, squaresize, squaresize))
-            else:
-                win.blit(straberry, (self.x * squaresize, self.y * squaresize))
     
-    pop = True
-
     lists2 = []
 
     lists2.append(fruitblock(random.randint(1, sizex-2), random.randint(1, sizey-2), (0, 225, 0)))
@@ -116,28 +105,33 @@ def snake():
 
     global lists
 
-    randomx = random.randint(6, sizex - 7)
-    randomy = random.randint(6, sizey - 7)
+    xbuffer = 4
+    ybuffer = 4
+
+    randomx = random.randint(xbuffer, sizex - (1 + xbuffer))
+    randomy = random.randint(ybuffer, sizey - (1 + ybuffer))
 
     orentation = random.randint(0, 3)
 
     if orentation ==  0:
-        lists = [snakeblock(randomx - 3, randomy), snakeblock(randomx - 2, randomy), snakeblock(randomx - 1, randomy), snakeblock(randomx, randomy)]
+        lists = [snakeblock(0, randomy), snakeblock(0 + 1, randomy), snakeblock(0 + 2, randomy), snakeblock( 0 + 3, randomy)]
         direction  = "right"
     elif orentation ==  1:
-        lists = [snakeblock(randomx, randomy), snakeblock(randomx - 1, randomy), snakeblock(randomx - 2, randomy), snakeblock(randomx - 3, randomy)]
+        lists = [snakeblock(sizex - 1, randomy), snakeblock(sizex - 2, randomy), snakeblock(sizex - 3, randomy), snakeblock(sizex - 4, randomy)]
         direction  = "left"
     elif orentation ==  2:
-        lists = [snakeblock(randomx, randomy), snakeblock(randomx, randomy - 1), snakeblock(randomx, randomy - 2 ), snakeblock(randomx, randomy - 3)]
+        lists = [snakeblock(randomx, sizey - 1), snakeblock(randomx, sizey - 2), snakeblock(randomx, sizey - 3), snakeblock(randomx, sizey - 4)]
         direction  = "up"
     elif orentation ==  3:
-        lists = [snakeblock(randomx, randomy - 3), snakeblock(randomx, randomy - 2), snakeblock(randomx, randomy - 1), snakeblock(randomx, randomy)]
+        lists = [snakeblock(randomx, 0), snakeblock(randomx, 0 + 1), snakeblock(randomx, 0 + 2), snakeblock(randomx, 0 + 3)]
         direction  = "down"
 
+ 
+    pop = True
     sets = set()
 
-    delay = 30
-    count = 0
+    delay = 150
+    timekeeper = time.time()*1000
 
     run = True
     while run:
@@ -149,7 +143,9 @@ def snake():
                 pygame.quit()
                 quit()
 
-        pygame.time.delay(delay)
+        pygame.time.delay( int(delay - ((time.time() * 1000) - timekeeper)))
+
+        timekeeper = time.time()*1000
 
         keys = pygame.key.get_pressed()
             
@@ -164,80 +160,58 @@ def snake():
                     run = False
                 lists.append(snakeblock(lists[-1].x - 1, lists[-1].y))
                 direction = "left"
-                if pop == True :   
-                    lists.pop(0)
-                else:
-                    pop = True
-
+                
             if keys[pygame.K_UP]:
                 if lists[-1].y == 0:
                     run = False
                 lists.append(snakeblock(lists[-1].x, lists[-1].y - 1))
                 direction = "up"
-                if pop == True :   
-                    lists.pop(0)
-                else:
-                    pop = True
-
+                
             if keys[pygame.K_DOWN]:
                 if lists[-1].y == sizey - 1:
                     run = False
                 lists.append(snakeblock(lists[-1].x, lists[-1].y + 1))
                 direction = "down"
-                if pop == True :   
-                    lists.pop(0)
-                else:
-                    pop = True
-            
+                
             if keys[pygame.K_RIGHT]:
                 if lists[-1].x == sizex - 1:
                     run = False
                 lists.append(snakeblock(lists[-1].x + 1, lists[-1].y))
                 direction = "right"
-                if pop == True :   
-                    lists.pop(0)
-                else:
-                    pop = True
-
+                
         else:
 
             if direction == "left" :
                 if lists[-1].x == 0:
                     run = False
                 lists.append(snakeblock(lists[-1].x - 1, lists[-1].y))
-                direction = "left"
-                
 
             if direction == "up" :
                 if lists[-1].y == 0:
                     run = False
                 lists.append(snakeblock(lists[-1].x, lists[-1].y - 1))
-                direction = "up"
-                
 
             if direction == "down" :
                 if lists[-1].y == sizey - 1:
                     run = False
                 lists.append(snakeblock(lists[-1].x, lists[-1].y + 1))
-                direction = "down"
-                
-            
+
             if direction == "right":
                 if lists[-1].x == sizex - 1:
                     run = False
                 lists.append(snakeblock(lists[-1].x + 1, lists[-1].y))
-                direction = "right"
 
-            if pop == True :   
-                lists.pop(0)
-            else:
-                pop = True
+
+        if pop == True :   
+            lists.pop(0)
+        else:
+            pop = True
 
         if (lists[-1].x == lists2[0].x ) and (lists[-1].y == lists2[0].y):
             pop = False
             lists2.clear()
             lists2.append(fruitblock(random.randint(0, sizex-1), random.randint(0, sizey-1), (0, 225, 0)))
-            if random.randint(0, 200) == 0:
+            if random.randint(0, 300) == 0:
                 lists2[0].easteregg = True
             else:
                 lists2[0].easteregg = False
@@ -264,11 +238,6 @@ def snake():
 
         for x in range( 0 , sizey + 1):
             pygame.draw.line( win , color, ( x * squaresize, 0), ( x * squaresize, winxlength), linewidth) 
-
-        if delay != 0: 
-            if count % 4 == 0 :
-                delay = delay - 1
-            count = count + 1 
 
         pygame.display.update() 
     
